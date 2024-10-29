4a83ac895c9fb3963bd711de6e6d31cad292a6f4
caduamorimm-dev <amorimm.dev@gmail.com>
2024-10-28 22:46:14 -0300

    feat: add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,20 @@ export default function Home() {
       {loading ? (
         <SplashScreen />
       ) : (
-        <div>
+
+        <><div>
           <Topbar />
         </div>
+          <div className="flex justify-center">
+            <div className="flex justify-center px-4 sm:px-6 lg:px-8">
+              <div className="text-center">
+                <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold  mt-8 sm:mt-12 lg:mt-16 max-w-3xl mx-auto">
+                  Conecte-se com tutores: aprendizado de estudante para estudante
+                </h1>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,48 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import { Avatar } from '@/components/ui/avatar';
+import { User, Menu, X } from 'lucide-react';
+import { Separator } from '../ui/separator';
+
 const Topbar: React.FC = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleLogout = () => {
         console.log('Logout');
     };
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="bg-white border-b border-gray-200 shadow-md">
-            <div className="container mx-auto flex items-center justify-between p-4">
-                <div className="text-xl font-semibold text-gray-900">
-                    <Link href="/" className="text-gray-700 hover:text-gray-900">
-                        Logo
+            <div className="container mx-auto p-4">
+                <div className="flex items-center justify-between">
+                    {/* Logo */}
+                    <Link href="/" className="text-xl text-blue-700 font-semibold">
+                        Tutor Connect
                     </Link>
+
+                    {/* Mobile Menu Button */}
+                    <div className="lg:hidden">
+                        <Button
+                            variant="ghost"
+                            className="p-2"
+                            onClick={toggleMenu}
+                        >
+                            {isMenuOpen ? (
+                                <X className="h-6 w-6" />
+                            ) : (
+                                <Menu className="h-6 w-6" />
+                            )}
+                        </Button>
+                    </div>
+
+                    {/* Desktop Navigation */}
+                    <div className="hidden lg:flex items-center space-x-4">
+                        <Link href="/sign-in" className="text-black hover:text-blue-600 font-semibold">
+                            Inicio
+                        </Link>
+                        <Link href="/about" className="text-black hover:text-blue-600 font-semibold">
+                            Encontrar tutores
+                        </Link>
+                        <Link href="/TutorApplicationForm" className="bg-blue-700 text-white px-6 py-3 rounded-2xl hover:bg-blue-500 transition duration-300 font-semibold">
+                            Tornar-se Tutor
+                        </Link>
+
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="ghost" className="p-0">
+                                    <User className="h-8 w-8 text-blue-700" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <Link href="/profile" passHref>
+                                    <DropdownMenuItem>Acessar Perfil</DropdownMenuItem>
+                                </Link>
+                                <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
                 </div>
-                <div className="flex items-center space-x-4">
-                    <Link href="/sign-in" className="text-gray-700 hover:text-gray-900">
-                        Home
-                    </Link>
-                    <Link href="/about" className="text-gray-700 hover:text-gray-900">
-                        About
-                    </Link>
-                    <Link href="/services" className="text-gray-700 hover:text-gray-900">
-                        Services
-                    </Link>
-                    <Link href="/contact" className="text-gray-700 hover:text-gray-900">
-                        Contact
-                    </Link>
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="ghost" className="p-0">
-                                <Avatar className="w-8 h-8" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <Link href="/profile" passHref>
-                                <DropdownMenuItem>Acessar Perfil</DropdownMenuItem>
-                            </Link>
-                            <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
+
+                {/* Mobile Menu */}
+                <div className={`lg:hidden ${isMenuOpen ? 'block' : 'hidden'} pt-4`}>
+                    <Separator className="mb-5" />
+                    <div className="flex flex-col space-y-4">
+                        <Link href="/" className="text-black hover:text-blue-600 font-semibold">
+                            Inicio
+                        </Link>
+                        <Link href="/about" className="text-black hover:text-blue-600 font-semibold">
+                            Encontrar tutores
+                        </Link>
+
+                        <Link href="/profile" className="text-black hover:text-blue-600 font-semibold">
+                            Acessar Perfil
+                        </Link>
+
+                        <Link href="/TutorApplicationForm" className="bg-blue-700 text-white px-6 py-3 rounded-2xl hover:bg-blue-500 transition duration-300 text-center font-semibold">
+                            Tornar-se Tutor
+                        </Link>
+
+                        <Separator className="mb-5" />
+
+                        <Button onClick={handleLogout} className="text-black bg-white hover:bg-white hover:text-blue-600 font-semibold text-left">
+                            Logout
+                        </Button>
+                    </div>
                 </div>
             </div>
         </nav>
